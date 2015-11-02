d89bbc34eacf7b18e92dc3e540688c7329d684bf
Petar Ivanov <pepincho95@gmail.com>
2015-11-02 22:55:55 +0200

    add user_login, user_logout in website/view

--- thor/website/views.py
+++ thor/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -9,20 +9,35 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-	return render(request, "index.html")
+    return render(request, "index.html")
 
 
 def user_login(request):
-	return render(request, "login.html")
+    if request.user.is_authenticated():
+        return redirect("index")
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("index")
+            else:
+                return redirect("login")
+        else:
+            return render(request, "login.html")
 
 
 def user_logout(request):
-	pass
+    logout(request)
+    return redirect("index")
 
 
 def _validate_register(username, email, password, password2):
-	pass
+    pass
 
 
 def user_register(request):
-	return render(request, "register.html")	
+    return render(request, "register.html")
