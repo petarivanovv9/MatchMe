21a07f02941bf7263344f17655b056997d81fb1d
Petar Ivanov <pepincho95@gmail.com>
2015-11-02 19:19:22 +0200

    add login, register page

--- thor/website/views.py
+++ thor/website/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
 def index(request):
 	return render(request, "index.html")
-	
+
+
+def user_login(request):
+	pass
+
+
+def user_logout(request):
+	pass
+
+
+def _validate_register(username, email, password, password2):
+	pass
+
+
+def user_register(request):
+	pass	
