eeca43c5fe8bdf7119e6843878177663f2996291
Petar Ivanov <pepincho95@gmail.com>
2015-11-02 23:02:06 +0200

    add user_register in view.py

--- thor/website/views.py
+++ thor/website/views.py
@@ -36,8 +36,39 @@ def user_logout(request):
 
 
 def _validate_register(username, email, password, password2):
-    pass
+    if username is None or username.strip() == "":
+        return False
+
+    if email is None or email.strip() == "":
+        return False
+
+    if password is None or password.strip() == "" or password != password2:
+        return False
+
+    return True
 
 
 def user_register(request):
-    return render(request, "register.html")
+	if request.user.is_authenticated():
+		return redirect("index")
+	else:
+		if request.method == "POST":
+			username = request.POST.get("username")
+			email = request.POST.get("email")
+			password_1 = request.POST.get("password")
+			password_2 = request.POST.get("password2")
+			
+			if not _validate_register(username, email, password, password2):
+                return redirect("register")
+
+            user = User.objects.create_user(username, email, password)
+
+            if user is not None:
+            	user = authenticate(username=username, password=password)
+            	login(request, user)
+            	return redirect("index")
+            else:
+            	return render(request, "register.html")
+
+		else:
+			return render(request, "register.html")
