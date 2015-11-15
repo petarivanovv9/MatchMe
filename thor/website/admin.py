3b3bc472a0f041c5c3fafe2f84d4070691db636b
Petar Ivanov <pepincho95@gmail.com>
2015-11-15 18:26:46 +0200

    add city, place and event to admin

--- thor/website/admin.py
+++ thor/website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import City, Place, Event
+
+
 # Register your models here.
+
+
+admin.site.register(City)
+admin.site.register(Place)
+admin.site.register(Event)
