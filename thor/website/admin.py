c0649737ff17f5c6e1e86d7d2d19d75775989a16
Petar Ivanov <pepincho95@gmail.com>
2015-12-14 19:23:22 +0200

    Add UserProfile model to admin panel.

--- thor/website/admin.py
+++ thor/website/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import City, Place, Event
+from .models import City, Place, Event, UserProfile
 from .models import EventUser
 
 
@@ -10,5 +10,7 @@ from .models import EventUser
 admin.site.register(City)
 admin.site.register(Place)
 admin.site.register(Event)
+
+admin.site.register(UserProfile)
 
 admin.site.register(EventUser)
